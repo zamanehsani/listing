2791f7a381fedd7106ebafa1242b95045b17aecf
Zaman Ehsani <zamanehsani@gmail.com>
2025-02-06 20:38:08 +0400

    renamed the listing file and made the main layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Home page
+    <div className="max-w-8xl h-full w-full px-2 lg:px-24">
+      <main className="flex gap-x-6">
+        {/* add sidebar drower here
+         */}
+        <div className="w-full flex-1 flex-col">
+          {/* /main content here... */}
+          {/* header first  and main contain second*/}
+        </div>
       </main>
     </div>
   );
